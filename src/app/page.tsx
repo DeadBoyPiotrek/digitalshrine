716ed0738dee210c9caa686115f770b481263719
piotrek <pipolito2115@gmail.com>
2023-06-17 01:03:58 +0200

    feat: add links tooltips styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,22 @@ import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen gap-28">
-      <div className="flex-1 flex flex-col items-end ">
-        <h1 className="text-4xl">digital shrine</h1>
-        <Icons />
+    <div className="flex flex-col sm:flex-row items-center justify-center w-screen h-screen sm:gap-28 gap-14">
+      <div className="flex-1 flex items-end justify-end">
+        <div>
+          <h1 className="text-4xl mb-2">Hi, I&apos;m Piotrek</h1>
+          <Icons />
+        </div>
       </div>
-      <div className="flex-1 flex ">
-        <Image
-          src={'https://github.com/DeadBoyPiotrek.png'}
-          width={200}
-          height={200}
-          alt="DeadBoyPiotrek"
-        />
+      <div className="flex-1 flex">
+        <div className="max-w-xs ">
+          <Image
+            src={'https://github.com/DeadBoyPiotrek.png'}
+            width={200}
+            height={200}
+            alt="DeadBoyPiotrek"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { AiFillGithub } from 'react-icons/ai';
 import { MdOutlineMailOutline } from 'react-icons/md';
 import { FiAlignLeft } from 'react-icons/fi';
+import { Tooltip } from 'react-tooltip';
 
 const handleClickCopy = async () => {
   await navigator.clipboard.writeText('piotr.turski.work@gmail.com');
@@ -11,13 +12,46 @@ const handleClickCopy = async () => {
 export const Icons = () => {
   return (
     <div className="flex items-center text-3xl w-40 gap-4 mt-2">
-      <Link href="https://github.com/DeadBoyPiotrek" target="_blank">
+      <Tooltip
+        id="email-tooltip"
+        place="bottom"
+        variant="light"
+        style={{ fontSize: '1rem' }}
+      />
+      <Tooltip
+        id="GitHub-tooltip"
+        place="bottom"
+        variant="light"
+        style={{ fontSize: '1rem' }}
+      />
+      <Tooltip
+        id="Blog-tooltip"
+        place="bottom"
+        variant="light"
+        style={{ fontSize: '1rem' }}
+      />
+
+      <Link
+        href="https://github.com/DeadBoyPiotrek"
+        target="_blank"
+        data-tooltip-content="GitHub"
+        data-tooltip-id="email-tooltip"
+      >
         <AiFillGithub />
       </Link>
-      <button onClick={handleClickCopy}>
+      <button
+        onClick={handleClickCopy}
+        data-tooltip-content="Click to copy"
+        data-tooltip-id="email-tooltip"
+      >
         <MdOutlineMailOutline />
       </button>
-      <Link href="https://digital-shrine.vercel.app" target="_blank">
+      <Link
+        href="https://digital-shrine.vercel.app"
+        target="_blank"
+        data-tooltip-content="Blog"
+        data-tooltip-id="email-tooltip"
+      >
         <FiAlignLeft />
       </Link>
     </div>
