50ccb094c28662e1b64c418c657e31be638a8e7d
piotrek <pipolito2115@gmail.com>
2023-06-29 01:10:48 +0200

    docs: update readme

--- src/components/imposter.tsx
+++ src/components/imposter.tsx
@@ -2,16 +2,15 @@
 
 import { Canvas } from '@react-three/fiber';
 import { Imposter3d } from './imposter3d';
-import { Environment, useEnvironment } from '@react-three/drei';
-import { Controls } from './controls';
+import { Environment, OrbitControls, useEnvironment } from '@react-three/drei';
 
 export const Imposter = () => {
   const env = useEnvironment({ files: '/among_us/studio.hdr' });
   return (
     <Canvas>
-      <Controls />
-      <pointLight position={[10, 10, 10]} />
-      <ambientLight intensity={0.05} />
+      <OrbitControls />
+      {/* <pointLight position={[10, 10, 10]} intensity={0.01} /> */}
+      {/* <ambientLight intensity={0.05} /> */}
 
       <Imposter3d />
 
