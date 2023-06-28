b567224c8d643ab7a56a5bffafb29ed7f5f9482c
piotrek <pipolito2115@gmail.com>
2023-06-29 00:29:46 +0200

    feat: change react spring to framer motion

--- src/components/imposter3d.tsx
+++ src/components/imposter3d.tsx
@@ -7,12 +7,15 @@ License: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 Source: https://sketchfab.com/3d-models/among-us-f391d46f6c814f85981ff0947b5bb68b
 Title: Among Us
 */
-import { useSpring, animated, useSpringValue } from '@react-spring/three';
+
+import { motion } from 'framer-motion-3d';
+
 import * as THREE from 'three';
 import React, { useEffect, useRef } from 'react';
-import { useGLTF, useAnimations, useEnvironment } from '@react-three/drei';
+import { useGLTF, useEnvironment } from '@react-three/drei';
 import { GLTF } from 'three-stdlib';
 import { useState } from 'react';
+import { useSpring } from 'framer-motion';
 type GLTFResult = GLTF & {
   nodes: {
     Object_7: THREE.SkinnedMesh;
@@ -36,21 +39,12 @@ type GLTFActions = Record<ActionName, THREE.AnimationAction>;
 
 export function Imposter3d(props: JSX.IntrinsicElements['group']) {
   const group = useRef<THREE.Group>();
-  const { nodes, materials, animations } = useGLTF(
-    '/among_us/scene.gltf'
-  ) as GLTFResult;
-  // const { actions } = useAnimations<GLTFActions>(animations, group);
-  //! animation mouse
-  const [rotation, setRotation] = useSpring(() => ({
-    x: 0,
-    y: 0,
-    config: { mass: 1, tension: 120, friction: 14 },
-  }));
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const { nodes, materials } = useGLTF('/among_us/scene.gltf') as GLTFResult;
+
   useEffect(() => {
     function handleMouseMove(event: MouseEvent) {
-      // setRotation({ x: event.clientX, y: event.clientY });
-      setCursorPosition({ x: event.clientX, y: event.clientY });
+      springPropsX.set(mapCursorPositionX(event.clientX));
+      springPropsY.set(mapCursorPositionY(event.clientY));
     }
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -70,34 +64,35 @@ export function Imposter3d(props: JSX.IntrinsicElements['group']) {
     const mappedX = (cursorPosition.x / rangeX) * 2 - 1 - 0.3;
     const mappedY = (cursorPosition.y / rangeY) * 2 - 1;
     return { x: mappedX * 1, y: mappedY * 0.5 };
-    // setRotation({ x: mappedX * 1, y: mappedY * 0.5 });
   }
+  const mapCursorPositionX = (cursorPositionX: number) => {
+    const rangeX = window.innerWidth;
+    const mappedX = (cursorPositionX / rangeX) * 2 - 1 - 0.3;
+    return mappedX * 1;
+  };
+  const mapCursorPositionY = (cursorPositionY: number) => {
+    const rangeY = window.innerHeight;
+    const mappedY = (cursorPositionY / rangeY) * 2 - 1;
+    return mappedY * 0.5;
+  };
 
-  const x = useSpringValue(mapCursorPosition(cursorPosition).x);
-  console.log(x);
-  //! animation mouse
-  // run animation
-  // useEffect(() => {
-  //   actions['Walking'].play();
-  // });
-  const envMap = useEnvironment({ files: '/among_us/studio.hdr' });
+  const springPropsX = useSpring(0, {
+    stiffness: 50,
+    damping: 10,
+  });
+  const springPropsY = useSpring(0, {
+    stiffness: 50,
+    damping: 10,
+  });
+
   return (
-    <animated.group
+    <motion.group
       ref={group}
       {...props}
       dispose={null}
       scale={2.5}
       position={[0, -1, 0]}
-      // rotation={[cursorPosition.x, cursorPosition.y / 2, 0]}
-
-      rotation={[
-        mapCursorPosition(cursorPosition).y,
-        // mapCursorPosition(cursorPosition).x,
-        x,
-        // rotation.x,
-        // rotation.y,
-        0,
-      ]}
+      rotation={[springPropsY, springPropsX, 0]}
     >
       <group name="Sketchfab_Scene">
         <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
@@ -151,7 +146,7 @@ export function Imposter3d(props: JSX.IntrinsicElements['group']) {
           </group>
         </group>
       </group>
-    </animated.group>
+    </motion.group>
   );
 }
 
